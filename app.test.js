357e3292894fc1c1f1adfed2c023ab8b90a21ef2
Dan Benson <dbenson3091@gmail.com>
2022-09-11 14:14:35 -0500

    updated app to check for test if errors were performed correctly, updated seed file for more elaborate instructions

---
+++ app.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest");
+
+const app = require("./app");
+const db = require("./db");
+
+describe("test connection of app", function() {
+    test("not found site 404", async function() {
+        const response = await request(app).get('/no-link');
+        expect(response.statusCode).toEqual(404);
+    })
+});
+
+afterAll(function () {
+    db.end();
+  });
+  
--- foodie-love-seed.sql
+++ foodie-love-seed.sql
@@ -12,9 +12,9 @@ VALUES ('testuser',
         'dbenson3091@gmail.com',
         TRUE);
 
-INSERT INTO recipes (recipe_name, prep_time, cooking_time, recipe_image, meal_type)
-VALUES ('lemon tahini quinoa salad', 5, 25, NULL, 'vegan'),
-       ('savory spaghetti squash', 25, 45, NULL, 'vegan');
+INSERT INTO recipes (recipe_name, prep_time, cooking_time, recipe_image, instructions, meal_type)
+VALUES ('lemon tahini quinoa salad', 5, 25, NULL, '1. Heat 2 cups water and quinoa until boiling in medium saucepan over high heat, reduce heat to medium-low and simmer until water is absorbed and quinoa fluffs up, about 15 minutes. Quinoa is done when it is tender and there is a pop to each bite. Drain water and place quinoa in mixing bowl 2. Add onion, broccoli, peppers, tomoatoes, and chickpeas 3. To make the sauce, whisk together all the ingredients 4. Add the sauce to the cooked quinoa and vegetables 5. Serve garnished with a bit of cilantro', 'vegan'),
+       ('savory spaghetti squash', 25, 45, NULL, '1. Preheat oven to 350°F 2. Cut squash lengthwise and clean out seeds. Place squash cut sides down on a nonstick baking sheet. Bake for 45 minutes or until the squash is easily pierced with a sharp knife. Remove squash from oven and set aside to cool enough to be easily handled 3. While squash is baking, sauté onion and garlic with 2 tablespoons vegetable broth in a medium-sized skillet over medium heat until onion is soft. Add remaining vegetable broth, tomatoes, pepper, spices, and lemon juice. Cook for 5-8 minutes. Season with salt and pepper. Set aside. 4 Using a fork, gently pull the strands of squash away from the peel. Place the strands onto a large serving bowl 5. Add tomato mixture to squash and mix gently. Serve warm.', 'vegan');
 
 INSERT INTO ingredients (ingredient_name)
 VALUES ('spaghetti squash'), ('vegetable broth'), ('onion'), ('cloves garlic'), 

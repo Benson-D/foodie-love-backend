b33d8787cfc7945842961b56ae28170992bf82d7
Dan Benson <dbenson3091@gmail.com>
2022-12-20 21:31:43 -0600

    created controller folder to handle logic of routes

---
+++ src/controller/recipe.js
@@ -0,0 +1,81 @@
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../error/expressError");
+const { uploadRecipeImage } = require("../aws/s3");
+const Recipe = require("../models/recipe.js");
+
+const recipeNewSchema = require("../schemas/recipeNew.json");
+const recipeSearchSchema = require("../schemas/recipeSearch.json");
+
+async function recipeCreate(req, res, next) {
+    const validator = jsonschema.validate(req.body, recipeNewSchema);
+
+    if(!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        console.log(errs, 'error');
+        return res.status(400).json({ errors: errs });
+    }
+
+    const recipe = await Recipe.insertRecipe(req.body);
+    
+    const { ingredientList } = req.body; 
+    const recipeIngregients = JSON.parse(ingredientList);
+
+    recipe.ingredients = await Promise.all(recipeIngregients.map( 
+        async (list) => await Recipe._ingredientBuilder(list, recipe.id)));
+    
+    return res.status(201).json({ recipe });
+}
+
+async function recipeGet(req, res) {
+    const recipeQuery = req.query; 
+
+    const validator = jsonschema.validate(recipeQuery, recipeSearchSchema);
+
+    if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        return res.status(400).json({ errors: errs });
+    }
+
+    if (recipeQuery?.cookingTime) {
+        recipeQuery.cookingTime = Number(recipeQuery.cookingTime);
+    };
+
+    const recipes = await Recipe.findAll(recipeQuery);
+    return res.json({ recipes });
+}
+
+async function recipeGetIndividual(req, res) {
+    const recipe = await Recipe.getRecipe(req.params.id);
+    return res.json({ recipe });
+}
+
+async function recipeUploadPhoto(req, res) {
+    const image = req.file || '';
+    const urlResponse = await uploadRecipeImage(image);
+
+    if (image) await unlinkFile(image.path);
+
+    return res.status(201).json({ url: urlResponse });
+}
+
+async function recipeUpdate(req, res) {
+    const recipe = await Recipe.handleUpdates(req.params.id, req.body);
+    return res.json({ recipe });
+}
+
+async function recipeDelete(req, res) {
+    await Recipe.removeRecipe(req.params.id);
+    return res.json({ deleted: req.params.id });
+}
+
+
+module.exports = {
+    recipeCreate,
+    recipeGet,
+    recipeGetIndividual,
+    recipeUploadPhoto,
+    recipeUpdate,
+    recipeDelete
+}
--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,24 +1,19 @@
 "use strict";
 
 /** Routes for recipes. */
-const jsonschema = require("jsonschema");
 const express = require("express");
 const multer  = require('multer')
+const { 
+    recipeCreate, 
+    recipeGet, 
+    recipeGetIndividual, 
+    recipeUploadPhoto,
+    recipeUpdate,
+    recipeDelete 
+} = require("../controller/recipe");
 
-const { BadRequestError } = require("../error/expressError");
-const { uploadRecipeImage } = require("../aws/s3");
-const Recipe = require("../models/recipe.js");
-
-const recipeNewSchema = require("../schemas/recipeNew.json");
-const recipeSearchSchema = require("../schemas/recipeSearch.json");
 const upload = multer({ dest: "uploads/"});
-
-const fs = require("fs");
-const util = require("util");
-const unlinkFile = util.promisify(fs.unlink);
-
 const router = new express.Router();
-
 
 /** POST / { recipe } =>  { recipe }
  * 
@@ -27,35 +22,9 @@ const router = new express.Router();
  *        req.fle: { recipeImage } 
  * 
  */
-router.post("/", async function (req, res, next) {
-    const validator = jsonschema.validate(req.body, recipeNewSchema);
+router.post("/", recipeCreate);
 
-    if(!validator.valid) {
-        const errs = validator.errors.map(e => e.stack);
-        console.log(errs, 'error');
-        return res.status(400).json({ errors: errs });
-    }
-
-    const recipe = await Recipe.insertRecipe(req.body);
-    
-    const { ingredientList } = req.body; 
-    const recipeIngregients = JSON.parse(ingredientList);
-
-    recipe.ingredients = await Promise.all(recipeIngregients.map( 
-        async (list) => await Recipe._ingredientBuilder(list, recipe.id)));
-    
-    return res.status(201).json({ recipe });
-
-});
-
-router.post("/image", upload.single('recipeImage'), async function(req, res) {
-    const image = req.file || '';
-    const urlResponse = await uploadRecipeImage(image);
-
-    if (image) await unlinkFile(image.path);
-
-    return res.status(201).json({ url: urlResponse });
-}); 
+router.post("/image", upload.single('recipeImage'), recipeUploadPhoto); 
 
 /** GET / =>  
  *
@@ -70,23 +39,7 @@ router.post("/image", upload.single('recipeImage'), async function(req, res) {
  * Authorization required: none
  * 
  */
-router.get("/", async function (req, res) {
-    const recipeQuery = req.query; 
-
-    const validator = jsonschema.validate(recipeQuery, recipeSearchSchema);
-
-    if (!validator.valid) {
-        const errs = validator.errors.map(e => e.stack);
-        return res.status(400).json({ errors: errs });
-    }
-
-    if (recipeQuery?.cookingTime) {
-        recipeQuery.cookingTime = Number(recipeQuery.cookingTime);
-    };
-
-    const recipes = await Recipe.findAll(recipeQuery);
-    return res.json({ recipes });
-});
+router.get("/", recipeGet);
 
 /** GET /[id]  =>  { recipe }
  *
@@ -95,26 +48,17 @@ router.get("/", async function (req, res) {
  *
  * Authorization required: none
  */
-router.get("/:id", async function (req, res) {
-    const recipe = await Recipe.getRecipe(req.params.id);
-    return res.json({ recipe });
-});
+router.get("/:id", recipeGetIndividual);
 
 
 /** PATCH /[id] 
  * 
  */
-router.patch("/:id", async function(req, res) {
-    const recipe = await Recipe.handleUpdates(req.params.id, req.body);
-    return res.json({ recipe });
-})
+router.patch("/:id", recipeUpdate);
 
 /** DELETE /[id] => { deleted: id } 
  * 
  */
-router.delete("/:id", async function (req, res) {
-    await Recipe.removeRecipe(req.params.id);
-    return res.json({ deleted: req.params.id });
-})
+router.delete("/:id", recipeDelete);
 
 module.exports = router; 

ce9ba6faa97d1f560326a0aadb4e26bc34abcd7c
Dan Benson <dbenson3091@gmail.com>
2023-09-16 11:10:16 -0500

    updated middleware to typescript

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,24 +1,22 @@
-"use strict";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../configs/general";
+import { UnauthorizedError } from "../utils/expressError";
 
-/** Convenience middleware to handle common auth cases in routes. */
-const jwt = require("jsonwebtoken");
-const { SECRET_KEY } = require("../configs/general");
-const { UnauthorizedError } = require("../utils/expressError");
-
-/** Middleware: Authenticate user.
+/** 
+ * Middleware: Authenticate user.
  *
  * If a token was provided, verify it, and, if valid, store the token payload
  * on res.locals (this will include the username and isAdmin field.)
  *
  * It's not an error if no token was provided or if the token is not valid.
  */
-
-function authenticateJWT(req, res, next) {
+function authenticateJWT(req: Request, res: Response, next: NextFunction) {
   try {
     const authHeader = req.headers && req.headers.authorization;
     if (authHeader) {
       const token = authHeader.replace(/^[Bb]earer /, "").trim();
-      res.locals.user = jwt.verify(token, SECRET_KEY);
+      res.locals.user = jwt.verify(token, SECRET_KEY as string);
     }
     return next();
   } catch (err) {
@@ -26,20 +24,21 @@ function authenticateJWT(req, res, next) {
   }
 }
 
-/** Middleware to use when they must be logged in.
+/** 
+ * Middleware to use when they must be logged in.
  *
  * If not, raises Unauthorized.
  */
-function ensureLoggedIn(req, res, next) {
+function ensureLoggedIn(req: Request, res: Response, next: NextFunction) {
     try {
-        if(!res.locals,user) throw new UnauthorizedError();
+        if(!res.locals.user) throw new UnauthorizedError();
         return next();
     } catch(err) {
         return next(err);
     }
 }
 
-module.exports = {
+export {
     authenticateJWT,
     ensureLoggedIn,
 };

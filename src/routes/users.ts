6cc97c227d29851aa0d44554bb58a0cbbf15527f
Dan Benson <dbenson3091@gmail.com>
2023-09-17 20:04:15 -0500

    updated user routes with doc strings

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,30 +1,46 @@
 import UserModel from "../models/userModel";
 import { createToken } from "../utils/token";
-import  { Router } from "express";
+import { Router } from "express";
 
 const router: Router = Router();
 
+/**
+ * POST / { user }  => { user, token }
+ *
+ * Adds a new user and returns the newly created user and an authentication token for them:
+ * { user: { username, firstName, lastName, email, isAdmin }, token }
+ **/
 router.post("/", async function (req, res) {
-   const user = await UserModel.register(req.body);
-   const token = createToken(user); 
+  const user = await UserModel.register(req.body);
+  const token = createToken(user);
 
-   return res.status(201).json({ user, token });
+  return res.status(201).json({ user, token });
 });
 
+/**
+ * GET / => { users: [ {username, firstName, lastName, email }, ... ] }
+ * Returns list of all users.
+ **/
 router.get("/", async function (req, res, next) {
-    try {
-        const users = UserModel.findAll();
-        return res.json({ users });
-    } catch (err) {
-        return next(err)
-    }
+  try {
+    const users = UserModel.findAll();
+    return res.json({ users });
+  } catch (err) {
+    return next(err);
+  }
 });
 
-router.get("/:username", async function(req, res, next) {
-    try {
-        const user = UserModel.get(req.params.username);
-        return res.json({ user });
-    } catch (err) {
-        return next(err); 
-    }
+/**
+ * GET /[username] => { user }
+ * Returns a single user and their data/
+ **/
+router.get("/:username", async function (req, res, next) {
+  try {
+    const user = UserModel.get(req.params.username);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
 });
+
+export default router;

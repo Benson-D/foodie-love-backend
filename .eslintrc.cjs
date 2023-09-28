696c5cf8e8a83a434bd14ce38b65ebfe4554e71c
Dan Benson <dbenson3091@gmail.com>
2023-09-28 12:27:52 -0500

    created config file for eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+/* eslint-env node */
+const config = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
+
+module.exports = config;

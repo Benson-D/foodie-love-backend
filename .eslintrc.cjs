52c1a34b84ab0a2cc8b4c7132acd56a59183c372
Dan Benson <dbenson3091@gmail.com>
2024-02-08 03:46:02 -0600

    added check warnings of unused variables, and added warning for incorrect use of type key word

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,19 @@
 /* eslint-env node */
 const config = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   root: true,
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
 };
 
 module.exports = config;
